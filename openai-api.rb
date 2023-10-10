5f87d54793ac2ec12a3dde3e9157b678b5d3ca22
kei-600th <kei19941005@gmail.com>
2023-10-10 17:07:30 +0900

    ファイルの差分をopeaiに送信する

--- openai-api.rb
+++ openai-api.rb
@@ -6,11 +6,14 @@ api_key = ENV["OPENAI_API_KEY"]
 # OpenAIのクライアントを初期化
 client = OpenAI::Client.new(access_token: api_key)
 
+# Diff内容を読み込み
+diff_text = File.read("pr.diff")
+
 # GPT-3（またはGPT-4）でのテキスト生成リクエスト
 response = client.chat(
       parameters: {
         model: "gpt-4",
-        messages: [{ role: "user", content: "こんにちは!"}],
+        messages: [{ role: "user", content: "以下の修正箇所についてコメントしてください。\n" + diff_text }],
         temperature: 0.7
       }
     )
