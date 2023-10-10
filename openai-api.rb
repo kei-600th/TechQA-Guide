395761de12f020835ede0937c633db225ccaf639
kei-600th <kei19941005@gmail.com>
2023-10-10 15:29:50 +0900

    Update openai-api.rb

--- openai-api.rb
+++ openai-api.rb
@@ -1,12 +1,12 @@
 require 'openai'
 
-Openai.api_key = ENV["OPENAI_API_KEY"]
+OpenAI.api_key = ENV["OPENAI_API_KEY"]
 
 prompt = "こんにちは!"
 engine = "gpt-4"
 max_tokens = 100
 
-response = Openai::Completion.create(
+response = OpenAI::Completion.create(
   engine: engine,
   prompt: prompt,
   max_tokens: max_tokens
