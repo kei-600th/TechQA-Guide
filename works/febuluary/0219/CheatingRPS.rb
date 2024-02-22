c9ac96cfaff9039b65b0b9e2e913255cc5e03afa
kei-600th <kei19941005@gmail.com>
2024-02-22 20:37:12 +0900

    関数名変数名の変更

--- works/febuluary/0219/CheatingRPS.rb
+++ works/febuluary/0219/CheatingRPS.rb
@@ -10,21 +10,21 @@ def CheatingRPS(users_input)
   users_hand_index = HAND.index(users_input)
   program_hand_index = (users_hand_index - 1) % 3
 
-  MatchResult(users_hand_index, program_hand_index)
+  PrintJankenResult(users_hand_index, program_hand_index)
 end
 
 # じゃんけんの結果出力
-def MatchResult(users_hand_index, program_hand_index)
-  # 勝敗判定関数の結果をoutcomeに代入
-  outcome = DetermineOutcome(users_hand_index, program_hand_index)
+def PrintJankenResult(users_hand_index, program_hand_index)
+  # 勝敗判定関数の結果をwinnerに代入
+  winner = CalculateJankenWinner(users_hand_index, program_hand_index)
 
   puts "あなたが出した手は: #{HAND[users_hand_index]}"
   puts "私が出した手は: #{HAND[program_hand_index]}"
 
-  case outcome
-  when "program_win"
+  case winner
+  when "program"
     puts "私の勝ちです"
-  when "user_win"
+  when "user"
     puts "あなたの勝ちです"
   else
     puts "引き分けです"
@@ -32,12 +32,12 @@ def MatchResult(users_hand_index, program_hand_index)
 end
 
 # じゃんけんの勝敗判定
-def DetermineOutcome(users_hand_index, program_hand_index)
+def CalculateJankenWinner(users_hand_index, program_hand_index)
   case [users_hand_index, program_hand_index]
   when [0, 2], [1, 0], [2, 1] #[user,program]→[グー,パー],[チョキ,グー],[パー,チョキ]
-    "program_win"
+    "program"
   when [0, 1], [1, 2], [2, 0] #[user,program]→[グー,チョキ],[チョキ,パー],[パー,グー]
-    "user_win"
+    "user"
   else
     "draw"
   end
