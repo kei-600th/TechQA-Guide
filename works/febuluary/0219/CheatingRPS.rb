4e43daf076623383c0820695414a8cd8c951f06a
kei-600th <kei19941005@gmail.com>
2024-02-22 20:27:39 +0900

    じゃんけんの勝敗判定と出力を別々のメソッドに分ける

--- works/febuluary/0219/CheatingRPS.rb
+++ works/febuluary/0219/CheatingRPS.rb
@@ -13,19 +13,37 @@ def CheatingRPS(users_input)
   MatchResult(users_hand_index, program_hand_index)
 end
 
-# じゃんけんの勝敗判定
+# じゃんけんの結果出力
 def MatchResult(users_hand_index, program_hand_index)
+  # 勝敗判定関数の結果をoutcomeに代入
+  outcome = DetermineOutcome(users_hand_index, program_hand_index)
+
   puts "あなたが出した手は: #{HAND[users_hand_index]}"
   puts "私が出した手は: #{HAND[program_hand_index]}"
-  case [users_hand_index, program_hand_index]
-  when [0, 2], [1, 0], [2, 1] #[user,program]→[グー,パー],[チョキ,グー],[パー,チョキ]
+
+  case outcome
+  when "program_win"
     puts "私の勝ちです"
-  when [0, 1], [1, 2], [2, 0] #[user,program]→[グー,チョキ],[チョキ,パー],[パー,グー]
+  when "user_win"
     puts "あなたの勝ちです"
   else
     puts "引き分けです"
   end
 end
+
+# じゃんけんの勝敗判定
+def DetermineOutcome(users_hand_index, program_hand_index)
+  case [users_hand_index, program_hand_index]
+  when [0, 2], [1, 0], [2, 1] #[user,program]→[グー,パー],[チョキ,グー],[パー,チョキ]
+    "program_win"
+  when [0, 1], [1, 2], [2, 0] #[user,program]→[グー,チョキ],[チョキ,パー],[パー,グー]
+    "user_win"
+  else
+    "draw"
+  end
+end
+
+
 
 puts "グー、チョキ、パーのいずれかを入力してください："
 users_input = gets.chomp
